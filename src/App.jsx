b9a41a9732e102e71f14c18a680f84b63030e5ce
Joao Zenaro <joaomarcelo661@gmail.com>
2022-09-21 10:37:11 -0300

    setup firebase.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
 import Chat from "./components/Chat";
 import Navbar from "./components/Navbar";
 
-//import.meta.env.VITE_TEST_VAR
-
 export default function App() {
   return (
     <div className="mx-auto max-w-[800px] mt-6 shadow-lg bg-slate-800 p-5 rounded-lg">
