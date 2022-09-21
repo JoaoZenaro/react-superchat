4aacef298564aea78a3c1d172f7684dc65e8918f
Joao Zenaro <joaomarcelo661@gmail.com>
2022-09-21 10:44:27 -0300

    user logic app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import Chat from "./components/Chat";
 import Navbar from "./components/Navbar";
 
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from './firebase'
+
 export default function App() {
+  const [user] = useAuthState(auth)
+
   return (
     <div className="mx-auto max-w-[800px] mt-6 shadow-lg bg-slate-800 p-5 rounded-lg">
       <Navbar />
-      <Chat />
+      {user ? <Chat /> : null}
     </div>
   )
 }
