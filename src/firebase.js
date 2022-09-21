751a87255ad046ed8c5f67790c7eb948421341a4
Joao Zenaro <joaomarcelo661@gmail.com>
2022-09-21 10:47:59 -0300

    add vite to env vars firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,12 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
     // import.meta.env.VITE_TEST_VAR
-    apiKey: import.meta.env.APIKEY,
-    authDomain: import.meta.env.AUTHDOMAIN,
-    projectId: import.meta.env.PROJECTID,
-    storageBucket: import.meta.env.STORAGEBUCKET,
-    messagingSenderId: import.meta.env.MESSAGINGSENDERID,
-    appId: import.meta.env.APPID
+    apiKey: import.meta.env.VITE_APIKEY,
+    authDomain: import.meta.env.VITE_AUTHDOMAIN,
+    projectId: import.meta.env.VITE_PROJECTID,
+    storageBucket: import.meta.env.VITE_STORAGEBUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGINGSENDERID,
+    appId: import.meta.env.VITE_APPID
 };
 
 // Initialize Firebase
